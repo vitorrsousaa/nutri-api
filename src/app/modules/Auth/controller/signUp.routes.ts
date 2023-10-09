b148fa0893ac72c7c5222ccafccbf7b9741ac679
Vitor Sousa <v.sousa.cf@gmail.com>
2023-10-08 22:36:20 -0300

    fix: remove files

--- src/app/modules/Auth/controller/signUp.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import AuthModule from '../../module';
-
-const signUpRoute = Router();
-
-signUpRoute.post('/signUp', AuthModule.getController('signUp'));
-
-export default signUpRoute;
