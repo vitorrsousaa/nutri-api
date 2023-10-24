a1c7e81cff31ba25e1c5a16d639857b53c3fee7a
Vitor Sousa <v.sousa.cf@gmail.com>
2023-10-24 08:54:29 -0300

    fix: change middleware to major roule

--- src/app/modules/Patient/http/routes/create.routes.ts
+++ src/app/modules/Patient/http/routes/create.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from 'express';
 
-import authHandler from '../../../../shared/http/middlewares/authHandler';
 import PatientModule from '../../module';
 
 const createPatientRoutes = Router();
 
-createPatientRoutes.post(
-  '/create',
-  authHandler,
-  PatientModule.getController('create')
-);
+createPatientRoutes.post('/create', PatientModule.getController('create'));
 
 export default createPatientRoutes;
