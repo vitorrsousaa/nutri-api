e3f48a5fb4db80790f66ad121db6e8ed4429dbb2
Vitor Sousa <v.sousa.cf@gmail.com>
2023-10-29 19:48:05 -0300

    test: refactor patient test repositories

---
+++ src/app/shared/database/repositories/planningMeal/PlanningMealRepositories.spec.ts
@@ -0,0 +1,95 @@
+import { type PrismaClient } from '@prisma/client';
+
+import PlanningMealRepositores from './PlanningMealRepositories';
+
+describe('Planning Meal Repositories', () => {
+  let repository: PlanningMealRepositores;
+  let spy = {
+    findFirst: {} as jest.SpiedFunction<
+      PrismaClient['planningMeal']['findFirst']
+    >,
+    delete: {} as jest.SpiedFunction<PrismaClient['planningMeal']['delete']>,
+    create: {} as jest.SpiedFunction<PrismaClient['planningMeal']['create']>,
+  };
+
+  beforeEach(() => {
+    const prismaInstance = {
+      planningMeal: {
+        findFirst: jest.fn(),
+        delete: jest.fn(),
+        create: jest.fn(),
+      },
+    } as unknown as PrismaClient;
+
+    spy = {
+      findFirst: jest.spyOn(prismaInstance.planningMeal, 'findFirst'),
+      delete: jest.spyOn(prismaInstance.planningMeal, 'delete'),
+      create: jest.spyOn(prismaInstance.planningMeal, 'create'),
+    };
+
+    repository = new PlanningMealRepositores(prismaInstance);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call correcly create', async () => {
+    // Arrange
+    spy.create.mockResolvedValue({
+      id: 'id',
+      patientId: 'patient_id',
+      userId: 'user_id',
+    });
+
+    // Act
+    const result = await repository.create({
+      data: {
+        userId: 'user_id',
+        patientId: 'patient_id',
+      },
+    });
+
+    // Assert
+    expect(result).toEqual({
+      id: 'id',
+      patientId: 'patient_id',
+      userId: 'user_id',
+    });
+  });
+
+  it('should call correcly find first', async () => {
+    // Arrange
+    const findFirstMock = {
+      where: {
+        userId: 'user_id',
+        patientId: 'patient_id',
+      },
+    };
+
+    // Act
+    await repository.findFirst(findFirstMock);
+
+    // Assert
+    expect(spy.findFirst).toBeCalledWith(findFirstMock);
+  });
+
+  it('should call correcly delete', async () => {
+    // Arrange
+    const deleteMock = {
+      where: {
+        id: 'id',
+      },
+    };
+
+    // Act
+    await repository.delete({
+      where: {
+        id: 'id',
+      },
+    });
+
+    // Assert
+    expect(spy.delete).toBeCalledWith(deleteMock);
+  });
+});
