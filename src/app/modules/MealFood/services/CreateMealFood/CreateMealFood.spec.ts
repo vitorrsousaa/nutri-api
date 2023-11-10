a4ffe416eb71c89e641882c7e4bc4e7433ed81a9
Vitor Sousa <v.sousa.cf@gmail.com>
2023-11-10 08:43:12 -0300

    test: updated tests with new schema

--- src/app/modules/MealFood/services/CreateMealFood/CreateMealFood.spec.ts
+++ src/app/modules/MealFood/services/CreateMealFood/CreateMealFood.spec.ts
@@ -1,4 +1,5 @@
 import MealFoodRepositories from '../../../../shared/database/repositories/mealFood';
+import { ICreateMealFoodDTO } from '../../dtos/create-meal-food-dto';
 import { CreateMealFoodService } from './CreateMealFood';
 
 describe('Create meal food service', () => {
@@ -25,33 +26,56 @@ describe('Create meal food service', () => {
 
   it('should return created meal food', async () => {
     // Arrange
-    spy.create.mockResolvedValue({
+    const mealFoodMock = {
       foodId: 'any_food_id',
       mealId: 'any_meal_id',
       id: 'any_id',
       quantity: 100,
-    });
+      calories: 100,
+      carb: 100,
+      fat: 100,
+      foodOrigin: 'DATABASE',
+      name: 'any_name',
+      protein: 100,
+    };
+
+    spy.create.mockResolvedValue(mealFoodMock);
 
     // Act
-    const result = await service.execute(100, 'any_food_id');
+    const result = await service.execute({
+      calories: 100,
+      foodId: 'any_food_id',
+      quantity: 100,
+      carb: 100,
+      fat: 100,
+      foodOrigin: 'DATABASE',
+      name: 'any_name',
+      protein: 100,
+    });
 
     // Assert
-    expect(result).toEqual({
-      foodId: 'any_food_id',
-      mealId: 'any_meal_id',
-      id: 'any_id',
-      quantity: 100,
-    });
+    expect(result).toEqual(mealFoodMock);
   });
-  it('should return created meal food', async () => {
+
+  it('should call repository with correctly parameters', async () => {
     // Arrange
+    const createMealFoodMock = {
+      calories: 100,
+      foodId: 'any_food_id',
+      quantity: 100,
+      carb: 100,
+      fat: 100,
+      foodOrigin: 'DATABASE',
+      name: 'any_name',
+      protein: 100,
+    };
 
     // Act
-    await service.execute(100, 'any_food_id');
+    await service.execute(createMealFoodMock as ICreateMealFoodDTO);
 
     // Assert
     expect(spy.create).toHaveBeenCalledWith({
-      data: { foodId: 'any_food_id', quantity: 100 },
+      data: createMealFoodMock,
     });
   });
 });
--- src/app/modules/MealFood/services/CreateMealFood/CreateMealFood.ts
+++ src/app/modules/MealFood/services/CreateMealFood/CreateMealFood.ts
@@ -1,13 +1,23 @@
 import MealFoodRepositories from '../../../../shared/database/repositories/mealFood';
+import { ICreateMealFoodDTO } from '../../dtos/create-meal-food-dto';
 
 export class CreateMealFoodService {
   constructor(private readonly mealFoodRepositories: MealFoodRepositories) {}
 
-  async execute(quantity: number, foodId: string) {
+  async execute(createMealFoodDTO: ICreateMealFoodDTO) {
+    const { calories, carb, fat, foodId, foodOrigin, name, protein, quantity } =
+      createMealFoodDTO;
+
     const mealFood = await this.mealFoodRepositories.create({
       data: {
         quantity,
         foodId,
+        calories,
+        carb,
+        fat,
+        foodOrigin,
+        name,
+        protein,
       },
     });
 
