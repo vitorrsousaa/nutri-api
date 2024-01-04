07df240563872e9710ad414d8d97a33f054117e9
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-04 18:30:27 -0300

    chore: [NUTR-116] - Change swagger for production

--- src/app/index.ts
+++ src/app/index.ts
@@ -16,7 +16,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(routes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(errorHandler);
 
