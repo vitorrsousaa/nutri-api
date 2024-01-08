6b8605a7ff6a52541fe4fa45b7dae07286bb1946
Vitor Sousa <v.sousa.cf@gmail.com>
2024-01-07 23:36:26 -0300

    feat: [NUTR-122] - updated service when email is another patient

--- src/app/modules/Patient/services/Update/UpdateService.ts
+++ src/app/modules/Patient/services/Update/UpdateService.ts
@@ -1,4 +1,5 @@
 import PatientRepositories from '../../../../shared/database/repositories/patient';
+import { AppError } from '../../../../shared/error';
 import ValidatePatientOwnershipService from '../../../../shared/services/ValidatePatientOwnership';
 import ValidatePatientStatusService from '../../../../shared/services/ValidatePatientStatus';
 import { IUpdatePatientDTO } from '../../dtos/update-patient-dto';
@@ -12,6 +13,12 @@ interface IUpdatePatientServiceInput {
   patient: IUpdatePatientDTO;
   userId: string;
   patientId: string;
+}
+
+interface IValidatePatientServiceInput {
+  patientId: string;
+  userId: string;
+  patientEmail: string;
 }
 
 export interface IUpdateService {
@@ -33,7 +40,11 @@ export class UpdateService implements IUpdateService {
     const { patient, userId, patientId } = updateServiceInput;
     const { email, birthDate, gender, height, weight } = patient;
 
-    await this.validatePatient(userId, patientId);
+    await this.validatePatient({
+      userId,
+      patientId,
+      patientEmail: patient.email,
+    });
 
     const updatePatient = await this.patientRepositories.update({
       where: {
@@ -53,10 +64,27 @@ export class UpdateService implements IUpdateService {
     };
   }
 
-  private async validatePatient(userId: string, patientId: string) {
+  private async validatePatient(
+    validatePatientInput: IValidatePatientServiceInput
+  ) {
+    const { userId, patientId, patientEmail } = validatePatientInput;
     return Promise.all([
       this.validatePatientOwnershipService.validate(userId, patientId),
       this.validatePatientStatusService.validate(userId, patientId),
+      this.validateHasPatientWithEmail(patientEmail, patientId),
     ]);
+  }
+
+  private async validateHasPatientWithEmail(email: string, id: string) {
+    const patient = await this.patientRepositories.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (patient && patient.id !== id) {
+      console.log('aqui dentro');
+      throw new AppError('Patient already exists');
+    }
   }
 }
